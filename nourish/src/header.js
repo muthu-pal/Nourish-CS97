9742e22372cae1c78a41fe2c90980290c4fd8e8c
muthu-pal <muthupal@g.ucla.edu>
2020-11-08 09:46:33 -0800

    created upload button styling and intiial header component

---
+++ nourish/src/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css'
+
+function Header() {
+    return (
+      <div className="container">
+        <h1 className="title">Nourish</h1>
+        <button className="upload">UPLOAD</button>
+        <button classname="search">SEARCH</button>
+      </div>
+    )
+  }
+  
+  export default Header
