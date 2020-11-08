ef7adfe4ad47ff0a6cdb4c10714820fd812d7bef
muthu-pal <muthupal@g.ucla.edu>
2020-11-08 10:50:31 -0800

    created footer

--- nourish/src/App.js
+++ nourish/src/App.js
@@ -1,11 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './header'
+import Footer from './footer'
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1><h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <Footer />
     </div>
   );
 }
---
+++ nourish/src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+function Footer() {
+    return (
+      <div className="container-footer">
+        <h1 className="right-footer">Created by Bryan, Isaac, Muthu, and Sabrina</h1>
+      </div>
+    )
+  }
+  
+export default Footer
