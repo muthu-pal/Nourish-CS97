0a69e9182ac3d29720dc3d4e85be18e51b6d370a
muthu-pal <muthupal@g.ucla.edu>
2020-11-08 12:21:38 -0800

    fixed post component to take in props as data

--- nourish/src/App.js
+++ nourish/src/App.js
@@ -9,11 +9,25 @@ function App() {
     <div className="App">
         <Header />
         <div className="feed">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post image={"https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"}
+              title={"selling some cool produce from my garden at home i have lots of food"}
+              paragraph={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."}
+              tags={"produce, food, grocery"}
+              likes={49}
+              comments={"wow can i have some"} />
+        <Post image={"https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"}
+              title={"selling some produce"}
+              paragraph={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."}
+              tags={"produce, food, grocery"}
+              likes={49}
+              comments={"wow can i have some"} />
+        <Post image={"https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"}
+              title={"selling some produce"}
+              paragraph={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."}
+              tags={"produce, food, grocery"}
+              likes={49}
+              comments={"wow can i have some"} />
+        
         </div>
         <Footer />
     </div>
--- nourish/src/post.js
+++ nourish/src/post.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './post.css'
 
-function Post() {
+function Post(props) {
     return (
       <div className="container-post">
         <div className="post-image">
-            <img src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" alt="post"/>
+            <img src={props.image} alt="post"/>
         </div>
         <div className="post-content">
-          <h1 className="title-post">posts</h1>
-          <p className="paragraphs">hdokenckldencfklnekcfnweknncdiosjcojdej ccdkomncodsokcokdfmokvmdfomdckne</p>
-          <h5 className="tags">############</h5>
-          <h5 className="likes">45 likes</h5>
+          <h1 className="title-post">{props.title}</h1>
+          <p className="paragraphs">{props.paragraph}</p>
+          <h5 className="tags">Tags: {props.tags}</h5>
+          <h5 className="likes">{props.likes} likes</h5>
           <h5 className="comment-title">COMMENTS</h5>
-          <p className="comments">i like this</p>
+          <p className="comments">{props.comments}</p>
         </div>
       </div>
     )
   }
   
   export default Post
+
+  //"https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
