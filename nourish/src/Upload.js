e6a2027427e55fea1d85f5492a0c1a50c3a80b83
Sabrina Nguyen <sabrinanguyen23@yahoo.com>
2020-11-09 21:48:06 -0800

    added styling for upload page

--- nourish/src/Upload.js
+++ nourish/src/Upload.js
@@ -1,5 +1,4 @@
-
-import './App.css';
+import './Upload.css';
 import Header from './header'
 import Footer from './footer'
 import React from 'react';
@@ -38,47 +37,58 @@ export default class Upload extends React.Component {
         return( 
             <div>
                 <Header />
-          <form onSubmit={this.handleSubmit}> 
-            <div> 
-              <label htmlFor='title'>title</label> 
-              <input  
+                <div className="upload-container">
+          <form className="upload-form" onSubmit={this.handleSubmit}> 
+            <div className="input-div"> 
+              <label htmlFor='title'>Title:  </label> 
+              <input className="input"
                 name='title'
                 placeholder='title' 
                 value = {this.state.title} 
                 onChange={this.handleChange} 
               /> 
             </div> 
-            <div> 
-              <label htmlFor='caption'>caption</label> 
-              <input 
+            {/* <div className="input-div"> 
+              <label htmlFor='caption'>Caption:  </label> 
+              <input className="input" id="caption-input"
+                name='caption' 
+                placeholder='caption'
+                value={this.state.caption} 
+                onChange={this.handleChange} 
+              /> 
+            </div>  */}
+            <div className="input-div"> 
+              <label htmlFor='caption'>Caption:  </label> 
+              <textarea className="input" id="caption-input"
                 name='caption' 
                 placeholder='caption'
                 value={this.state.caption} 
                 onChange={this.handleChange} 
               /> 
             </div> 
-            <div> 
-              <label htmlFor='tags'>tags</label> 
-              <input 
+            <div className="input-div"> 
+              <label htmlFor='tags'>Tags:  </label> 
+              <input className="input"
                 name='tags' 
                 placeholder='tags'
                 value={this.state.tags} 
                 onChange={this.handleChange} 
               /> 
             </div> 
-            <div> 
-              <label htmlFor='image'>image</label> 
-              <input 
+            <div className="input-div"> 
+              <label htmlFor='image'>Image:  </label> 
+              <input className="input"
                 name='image' 
                 placeholder='image'
                 value={this.state.image} 
                 onChange={this.handleChange} 
               /> 
             </div> 
-            <div> 
-              <button>Submit</button> 
+            <div className="submit-div"> 
+              <button className="upload-button">Submit</button> 
             </div> 
           </form>
+          </div>
           <Footer />
           </div>
         ) 
