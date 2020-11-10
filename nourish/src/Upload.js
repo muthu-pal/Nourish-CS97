3ec9cc685fee694f4ae7b98e8f848933d229eaae
muthu-pal <muthupal@g.ucla.edu>
2020-11-09 20:43:01 -0800

    started to work on form building page

--- nourish/src/Upload.js
+++ nourish/src/Upload.js
@@ -2,16 +2,89 @@
 import './App.css';
 import Header from './header'
 import Footer from './footer'
+import React from 'react';
 
+export default class Upload extends React.Component {
+    constructor(props){ 
+        super(props) 
+        this.state = { title:'',caption:'', tags:'', image:''} 
+        this.handleChange = this.handleChange.bind(this) 
+        this.handleSubmit = this.handleSubmit.bind(this) 
+      } 
+      
+        
+      handleSubmit(event){ 
+        const { title, caption, tags, image } = this.state 
+        event.preventDefault() 
+        alert(` 
+          ____Your Details____\n 
+          title : ${title} 
+          caption : ${caption} 
+          tags : ${tags} 
+          image : ${image} 
+        `) 
+      } 
+      
+       
+      handleChange(event){ 
+        this.setState({ 
+           
+          [event.target.name] : event.target.value 
+        }) 
+      } 
+      
+      
+      render(){ 
+        return( 
+            <div>
+                <Header />
+          <form onSubmit={this.handleSubmit}> 
+            <div> 
+              <label htmlFor='title'>title</label> 
+              <input  
+                name='title'
+                placeholder='title' 
+                value = {this.state.title} 
+                onChange={this.handleChange} 
+              /> 
+            </div> 
+            <div> 
+              <label htmlFor='caption'>caption</label> 
+              <input 
+                name='caption' 
+                placeholder='caption'
+                value={this.state.caption} 
+                onChange={this.handleChange} 
+              /> 
+            </div> 
+            <div> 
+              <label htmlFor='tags'>tags</label> 
+              <input 
+                name='tags' 
+                placeholder='tags'
+                value={this.state.tags} 
+                onChange={this.handleChange} 
+              /> 
+            </div> 
+            <div> 
+              <label htmlFor='image'>image</label> 
+              <input 
+                name='image' 
+                placeholder='image'
+                value={this.state.image} 
+                onChange={this.handleChange} 
+              /> 
+            </div> 
+            <div> 
+              <button>Submit</button> 
+            </div> 
+          </form>
+          <Footer />
+          </div>
+        ) 
+      } 
+  }
 
-function Upload() {
-  return (
-    <div className="App">
-        <Header />
-        <h1>this is upload</h1>
-        <Footer />
-    </div>
-  );
-}
+  //title, caption, tags, picture
 
-export default Upload;
+  
