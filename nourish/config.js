a7bc45f37579154ec5cf1fe4c2455cb5d74a6441
ichettiath <ichettiath2019@g.ucla.edu>
2020-11-09 21:01:38 -0800

    Implemented creating and deleting posts

---
+++ nourish/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://luobryan:VoCi4LjH2ahbL2my@cluster0.bkt2g.mongodb.net/Nourish?retryWrites=true&w=majority',
+    SECRET_KEY: 'secret key thing'
+};
---
+++ nourish/graphql/resolvers/comments.js
@@ -0,0 +1,49 @@
+const Post = require("../../models/Post");
+const { UserInputError } = require("apollo-server");
+const checkAuth = require("../../util/check-auth");
+
+module.exports = {
+  Mutation: {
+    async createComment(_, { postId, body }, context) {
+      const { username } = checkAuth(context);
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment not allowed");
+      }
+      const post = await Post.findById(postId);
+      if (post) {
+        post.comments.unshift({
+          body: body,
+          username: username,
+          createdAt: new Date().toISOString(),
+        });
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+
+    async deleteComment(_, { postId, commentId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (!post) {
+          throw new Error("Post not found");
+        }
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+        if (commentIndex === -1) {
+          throw new Error("Comment not found");
+        }
+        if (post.comments[commentIndex].username !== user.username) {
+          throw new Error("Action not allowed");
+        }
+        const comment = post.comments[commentIndex];
+        post.comments.splice(commentIndex, 1);
+        await post.save();
+        return comment;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
