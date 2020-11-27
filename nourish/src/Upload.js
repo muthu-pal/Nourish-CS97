c8908f191dd6a66bdd91aa239c904988b5c6a47c
luobryan <bryanluo100@gmail.com>
2020-11-27 10:47:34 -0800

    tried to connect like button front/back end

--- nourish/src/Upload.js
+++ nourish/src/Upload.js
@@ -26,8 +26,6 @@ function Upload() {
       values.tagsString = ''
       values.imageName = ''
     }
-
-
   })
 
 
--- nourish/src/post.js
+++ nourish/src/post.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './post.css'
+import gql from 'graphql-tag';
+import {useMutation} from '@apollo/react-hooks';
+
+
 
 function Post(props) {
+  const [likePost] = useMutation(LIKE_POST,{
+    variables: { postId: id }
+  })
   //  const src = /;
   if(props.image!==''){
     return (
@@ -15,7 +22,7 @@ function Post(props) {
           <p className="paragraphs">{props.paragraph}</p>
           <h5 className="tags">Tags: {props.tags.toString()}</h5>
           <div className="likeBox">
-            <button className="likeButton">Like</button> 
+            <button className="likeButton" onClick={likePost()}>Like</button> 
             <h5 className="likes">{props.likes} likes</h5>
           </div>
           <h5 className="comment-title">COMMENTS</h5>
@@ -47,6 +54,16 @@ function Post(props) {
   }
   }
   
+  const LIKE_POST = gql`
+  mutation likePost($postId: ID!){
+    likePost(postId: $postId){
+      id
+      likes{
+        id username
+      }
+    }
+  }
+`
   export default Post
 
   //"https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
